62730bdbf2657bcaf95c46bce75c34e14f536d4f
mushymato <wildshinobu@gmail.com>
2026-09-13 22:48:39 -0400

    add gmcm

--- HatHairToggle/ModEntry.cs
+++ HatHairToggle/ModEntry.cs
@@ -1,38 +1,60 @@
-using System.Diagnostics;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
 using StardewValley;
 using StardewValley.Menus;
 using StardewValley.Objects;
 
 namespace HatHairToggle;
 
+public sealed class ModConfig
+{
+    public KeybindList ToggleKey = KeybindList.Parse("MouseRight,LeftStick");
+}
+
 public sealed class ModEntry : Mod
 {
-#if DEBUG
-    private const LogLevel DEFAULT_LOG_LEVEL = LogLevel.Debug;
-#else
-    private const LogLevel DEFAULT_LOG_LEVEL = LogLevel.Trace;
-#endif
-
     public const string ModId = "mushymato.HatHairToggle";
-    private static IMonitor mon = null!;
-    internal static IModHelper help = null!;
     private IBetterGameMenuApi? BGM;
+    private ModConfig config = new();
 
     public override void Entry(IModHelper helper)
     {
         I18n.Init(helper.Translation);
-        mon = Monitor;
-        help = helper;
+        config = helper.ReadConfig<ModConfig>();
 
-        help.Events.GameLoop.GameLaunched += OnGameLaunched;
-        help.Events.Input.ButtonsChanged += OnButtonsChanged;
+        helper.Events.GameLoop.GameLaunched += OnGameLaunched;
+        helper.Events.Input.ButtonsChanged += OnButtonsChanged;
     }
 
     private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
     {
         BGM = Helper.ModRegistry.GetApi<IBetterGameMenuApi>("leclair.bettergamemenu");
+        if (
+            Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu")
+            is IGenericModConfigMenuApi gmcm
+        )
+        {
+            gmcm.Register(
+                ModManifest,
+                reset: () =>
+                {
+                    config.ToggleKey = KeybindList.Parse("RightClick,LeftStick");
+                    Helper.WriteConfig(config);
+                },
+                save: () =>
+                {
+                    Helper.WriteConfig(config);
+                }
+            );
+            gmcm.AddKeybindList(
+                ModManifest,
+                () => config.ToggleKey,
+                (value) => config.ToggleKey = value,
+                I18n.Config_ToggleKey_Name,
+                I18n.Config_ToggleKey_Desc
+            );
+        }
     }
 
     public bool IsGameMenu(IClickableMenu menu)
@@ -66,9 +88,9 @@ public sealed class ModEntry : Mod
 
     private void OnButtonsChanged(object? sender, ButtonsChangedEventArgs e)
     {
-        if (!Game1.didPlayerJustRightClick())
-            return;
         if (Game1.player.hat.Value is not Hat hat)
+            return;
+        if (!config.ToggleKey.JustPressed())
             return;
         if (GetInventoryPage(Game1.activeClickableMenu) is not InventoryPage menu)
             return;
@@ -76,38 +98,14 @@ public sealed class ModEntry : Mod
         int y = Game1.getOldMouseY();
         foreach (ClickableComponent cc in menu.equipmentIcons)
         {
-            if (cc.containsPoint(x, y))
+            if (cc.name == "Hat" && cc.containsPoint(x, y))
             {
                 int hairDrawType = hat.hairDrawType.Value;
                 int newHairDrawType = (hairDrawType + 1) % 3;
                 hat.hairDrawType.Value = newHairDrawType;
+                Monitor.Log($"hat.hairDrawType.Value {hairDrawType} -> {newHairDrawType}");
                 return;
             }
         }
-    }
-
-    /// <summary>SMAPI static monitor Log wrapper</summary>
-    /// <param name="msg"></param>
-    /// <param name="level"></param>
-    internal static void Log(string msg, LogLevel level = DEFAULT_LOG_LEVEL)
-    {
-        mon.Log(msg, level);
-    }
-
-    /// <summary>SMAPI static monitor LogOnce wrapper</summary>
-    /// <param name="msg"></param>
-    /// <param name="level"></param>
-    internal static void LogOnce(string msg, LogLevel level = DEFAULT_LOG_LEVEL)
-    {
-        mon.LogOnce(msg, level);
-    }
-
-    /// <summary>SMAPI static monitor Log wrapper, debug only</summary>
-    /// <param name="msg"></param>
-    /// <param name="level"></param>
-    [Conditional("DEBUG")]
-    internal static void LogDebug(string msg, LogLevel level = DEFAULT_LOG_LEVEL)
-    {
-        mon.Log(msg, level);
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# HatHairToggle
+# Hat Hair Toggle
 
-New mod.
+Toggle how hair is displayed under your current hat by right-clicking the hat equip slot (rebindable). This mod works on any content patcher hat.
+
+The value persists even when you take your hat off and even if you uninstall this mod, but it is reset to the base value if you put the hat into a dresser.
+
+## Configuration
+
+- `ToggleKey`: Change the toggle key. Default is `MouseRight` and `LeftStick`.
+
+## Translations
+
+- English
+
+- 简体中文
